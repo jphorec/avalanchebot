b10350246eaff3f942f1a0158e3414f8516bb5ba
jphorec <jphorec@gmail.com>
2017-02-15 17:53:04 -0600

    try to regex out strong title

--- scripts/main.js
+++ scripts/main.js
@@ -4,87 +4,55 @@ var _       = require('lodash'),
 
 module.exports = function (avalanchebot) {
     avalanchebot.hear(/http/i, function (bot) {
+        var debug = false;
         console.log(bot.envelope.room);
         var message = bot.message;
         var url = message.text;
 
         var testUrl = url.match(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/);
-        var noLinkyUrl = url.match(/(<a href="([^"]+)">([^<]+)<\/a>)/);
-        var testTitle = url.match(/<a [^>]+>([^<]+)<\/a>/);
+        var strongTitle = url.match(/(<a href="([^"]+)">([^<]+)<\/a>|<a href="([^"]+)"><strong>([^<]+)<\/strong><\/a>)/);
+        var anchorTitle = url.match(/<a [^>]+>([^<]+)<\/a>/);
+        var imageTag = url.match(/(<img src="([^"]+)"([^<]+)\/>)/);
         //console.log(testUrl);
         //console.log(noLinkyUrl);
-        //console.log(testTitle);
-        var nonLinkyUrl = noLinkyUrl && noLinkyUrl[2];
+        console.log("anchorTitle: " + anchorTitle);
+        console.log("strongTitle: " + strongTitle);
+        console.log("ImageTag: " + imageTag);
         var onlyUrl = testUrl && testUrl[0];
-        var title = testTitle && testTitle[1];
+        var imageUrl = imageTag && imageTag[2];
+        var title = anchorTitle && anchorTitle[1];
+        if (!title) {
+            title = strongTitle && strongTitle[5];
+        }
         console.log("Title: " + title);
-
+        console.log("Image: " + imageUrl);
         console.log("URL:" + onlyUrl);
-        console.log("nonURL: " + nonLinkyUrl);
         createDate = new Date().toISOString();
         if (onlyUrl && title) {
-            request({
-                url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
-                method: 'PUT',
-                //Lets post the following key/values as form
-                json: {
-                    title: title,
-                    url: onlyUrl,
-                    thumbnailUrl: onlyUrl,
-                    author: 'jphorec',
-                    userId: 'J3SCeLpgDcS5RC9bT',
-                    createdAt: createDate,
-                    postedAt: createDate
-                }
-            }, function(error, response, body){
-                if(error) {
-                    console.log(error);
-                } else {
-                    console.log(response.statusCode, body);
-                }
-            });
-        } else if(onlyUrl) {
-            request({
-                url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
-                method: 'PUT',
-                //Lets post the following key/values as form
-                json: {
-                    title: onlyUrl,
-                    url: onlyUrl,
-                    thumbnailUrl: onlyUrl,
-                    author: 'jphorec',
-                    userId: 'J3SCeLpgDcS5RC9bT',
-                    createdAt: createDate,
-                    postedAt: createDate
-                }
-            }, function(error, response, body){
-                if(error) {
-                    console.log(error);
-                } else {
-                    console.log(response.statusCode, body);
-                }
-            });
-        } else if(nonLinkyUrl) {
-            request({
-                url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
-                method: 'PUT',
-                //Lets post the following key/values as form
-                json: {
-                    title: nonLinkyUrl,
-                    url: nonLinkyUrl,
-                    thumbnailUrl: nonLinkyUrl,
-                    author: 'jphorec',
-                    userId: 'J3SCeLpgDcS5RC9bT',
-                    createdAt: createDate,
-                    postedAt: createDate
-                }
-            }, function(error, response, body){
-                if(error) {
-                    console.log(error);
-                } else {
-                    console.log(response.statusCode, body);
-                }
-            });
+            if (!debug) {
+                request({
+                    url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
+                    method: 'PUT',
+                    //Lets post the following key/values as form
+                    json: {
+                        title: title,
+                        url: onlyUrl,
+                        thumbnailUrl: imageUrl,
+                        author: 'jphorec',
+                        userId: 'J3SCeLpgDcS5RC9bT',
+                        createdAt: createDate,
+                        postedAt: createDate
+                    }
+                }, function(error, response, body){
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        console.log(response.statusCode, body);
+                    }
+                });
+            } else {
+                console.log("should have sent request...");
+            }
         }
         return;
 
