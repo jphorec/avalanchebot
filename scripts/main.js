b9fed4293b604bc29343bfeda61a4273a78cf6a0
jphorec <jphorec@gmail.com>
2017-02-15 13:12:31 -0600

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -4,22 +4,24 @@ var _       = require('lodash'),
 
 module.exports = function (avalanchebot) {
     avalanchebot.hear(/http/i, function (bot) {
-        console.log(bot);
-        var message = bot.message;
-        var testUrl = message.match(/(http:[^\s]+)/);
+        console.log(bot.envelope.room);
+        var url = bot.match[0].input;
+        console.log(url);
+        /*var testUrl = message.match(/(http:[^\s]+)/);
         if (!testUrl) {
             testUrl = message.match(/(https:[^\s]+)/);
         }
         var onlyUrl = testUrl && testUrl[1];
-        console.log(onlyUrl);
-        if (onlyUrl) {
+        console.log("URL:" + onlyUrl);
+        */
+        if (url) {
             request({
                 url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
                 method: 'PUT',
                 //Lets post the following key/values as form
                 json: {
-                    title: onlyUrl,
-                    url: onlyUrl
+                    title: url,
+                    url: url
                 }
             }, function(error, response, body){
                 if(error) {
