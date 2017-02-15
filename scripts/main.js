331b93f0edd7d2626af220ec4c9b1701aee5199f
jphorec <jphorec@gmail.com>
2017-02-15 12:44:51 -0600

    submit to avalanche

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,32 @@ var _       = require('lodash'),
 
 
 module.exports = function (avalanchebot) {
-    avalanchebot.hear(/badjder/i, function (bot) {
+    avalanchebot.hear(/http/i, function (bot) {
         console.log(bot);
-        /*var message = bot.message;
+        var message = bot.message;
         var testUrl = message.match(/(http:[^\s]+)/);
         if (!testUrl) {
             testUrl = message.match(/(https:[^\s]+)/);
         }
         var onlyUrl = testUrl && testUrl[1];
         console.log(onlyUrl);
-        */
-        bot.send("howdy");
+        if (onlyUrl) {
+            request({
+                url: 'http://jphorec:password@slalom-avalanche.herokuapp.com/skibot/posts', //URL to hit
+                method: 'PUT',
+                //Lets post the following key/values as form
+                json: {
+                    title: onlyUrl,
+                    url: onlyUrl
+                }
+            }, function(error, response, body){
+                if(error) {
+                    console.log(error);
+                } else {
+                    console.log(response.statusCode, body);
+                }
+            });
+        }
         return;
 
     });
