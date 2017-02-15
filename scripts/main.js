e2feea8b831dfc5bb6554d6f0fc4448b50c7983e
jphorec <jphorec@gmail.com>
2017-02-15 11:57:56 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var _       = require('lodash'),
+    request = require('request');
+
+
+module.exports = function (avalanchebot) {
+    avalanchebot.hear(/http/i, function (bot) {
+        console.log(bot);
+        var message = bot.message;
+        var testUrl = message.match(/(http:[^\s]+)/);
+        if (!testUrl) {
+            testUrl = message.match(/(https:[^\s]+)/);
+        }
+        var onlyUrl = testUrl && testUrl[1];
+        console.log(onlyUrl);
+        return;
+
+    });
+};
